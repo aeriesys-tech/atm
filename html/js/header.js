38926a506549a2fe03568669efd50415ba0ca5f6
Bharatesh Shanawad <bharatesh.shanawad-v@adityabirla.com-at-718927010107>
2025-07-03 11:52:30 +0530

    html

---
+++ html/js/header.js
@@ -0,0 +1,9 @@
+const nav = document.querySelector('.header')
+fetch('/header.html')
+.then(res=>res.text())
+.then(data=>{
+    nav.innerHTML=data
+    const parser = new DOMParser()
+    const doc = parser.parseFromString(data, 'text/html')
+    eval(doc.querySelector('script').textContent)
+})
